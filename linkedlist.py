7ea8b99090d78aded7a5d67f759dfa881b348e14
EmmaSamoilova <emmasamojlova@gamil.com>
2020-11-20 17:48:38 +0200

    Added function add to head, add to end, get node, remove

--- linkedlist.py
+++ linkedlist.py
@@ -11,3 +11,38 @@ class Linkedlist:
     def __init__(self):
         self.head = None
         self.tail = None
+
+    def addtohead(self, element):
+        newnode = Node(element, self.head)
+        self.head = newnode
+        if self.tail is None:
+            self.tail = newnode
+
+    def addtoend(self, element):
+        newnode = Node(element)
+        if self.head is None or self.tail is None:
+            self.head = newnode
+            self.tail = newnode
+        self.tail.nextnode = newnode
+        self.tail = newnode
+
+    def getNode(self, value):
+        current = self.head
+        count = 0
+        while count <= value:
+            if count == value:
+                return current
+            count += 1
+            current = current.nextnode
+
+    def remove(self, value):
+        current = self.head
+        next = current.nextnode
+        count = 0
+        while count < value:
+            count += 1
+            current = next
+            next = current.nextnode
+            if count == (value-1):
+                next = next.nextnode
+                current.nextnode = next
